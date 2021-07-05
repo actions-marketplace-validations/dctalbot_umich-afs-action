bbc97f0219c868102f0b858e2459045ad3662606
dctalbot <dctalbot@umich.edu>
2021-07-05 01:07:39 -0400

    work on ssh stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+set -eu
+
+SSH_PATH="$HOME/.ssh"
+mkdir "$SSH_PATH"
+
+SSH_PRIVATE_KEY_PATH="$SSH_PATH/github_actions"
+echo "$1" > "$SSH_PRIVATE_KEY_PATH"
+chmod 600 "$SSH_PRIVATE_KEY_PATH"
+
 echo "Hello $2"
-rsync -r --delete $1 $2
+echo "Hello $3"
+# rsync -r --delete $2 $3
+rsync -r --delete -e 'ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no' $2 $3
 time=$(date)
 echo "::set-output name=time::$time"
