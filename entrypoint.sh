46b35bcd8c1fef60bf73f735ef21e14f13971c7f
dctalbot <dctalbot@umich.edu>
2021-07-05 01:57:57 -0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ SSH_KEY_PATH="$SSH_PATH/github_actions"
 echo "$1" > "$SSH_KEY_PATH"
 chmod 600 "$SSH_KEY_PATH"
 
-rsync -r --delete -e "sshpass -e ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no" $GITHUB_WORKSPACE $3
+rsync -r --delete -e "sshpass -e ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no" $GITHUB_WORKSPACE/. $3
 
 time=$(date)
 echo "::set-output name=time::$time"
