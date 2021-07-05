37e092fbf001fd51152b042fd38dbc90aa9428bc
dctalbot <dctalbot@umich.edu>
2021-07-05 01:55:11 -0400

    use gh workspace env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,11 @@ set -eu
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 
-SSH_PRIVATE_KEY_PATH="$SSH_PATH/github_actions"
-echo "$1" > "$SSH_PRIVATE_KEY_PATH"
-chmod 600 "$SSH_PRIVATE_KEY_PATH"
+SSH_KEY_PATH="$SSH_PATH/github_actions"
+echo "$1" > "$SSH_KEY_PATH"
+chmod 600 "$SSH_KEY_PATH"
 
-echo "Hello $2"
-echo "Hello $3"
-# rsync -r --delete $2 $3
-rsync -r --delete -e "sshpass -e ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no" $2 $3
-# sh -c "rsync -r --delete -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $3"
+rsync -r --delete -e "sshpass -e ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no" $GITHUB_WORKSPACE $3
 
 time=$(date)
 echo "::set-output name=time::$time"
