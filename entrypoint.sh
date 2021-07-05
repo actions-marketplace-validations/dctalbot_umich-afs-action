6c5ffb63ccf6cd3644f59a09be53b3d13d49ac9d
dctalbot <dctalbot@umich.edu>
2021-07-05 01:11:24 -0400

    try double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ chmod 600 "$SSH_PRIVATE_KEY_PATH"
 echo "Hello $2"
 echo "Hello $3"
 # rsync -r --delete $2 $3
-rsync -r --delete -e 'ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no' $2 $3
+rsync -r --delete -e "ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no" $2 $3
+# sh -c "rsync -r --delete -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $3"
+
 time=$(date)
 echo "::set-output name=time::$time"
